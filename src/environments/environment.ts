6bead60553d59b5968a549c120ccba522705dc63
Lesly <105396883+Lesly-liseth@users.noreply.github.com>
2023-06-28 20:38:12 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,21 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    // apiKey: "AIzaSyDo3gA5ayjW2uY36Jzow-oLL96ddA7cM98",
-    // authDomain: "app2022b.firebaseapp.com",
-    // databaseURL: "https://app2022b-default-rtdb.firebaseio.com",
-    // projectId: "app2022b",
-    // storageBucket: "app2022b.appspot.com",
-    // messagingSenderId: "430051088078",
-    // appId: "1:430051088078:web:d2489c60cfcc8d3af0629f",
-    // measurementId: "G-18BCZEN1EZ"
-    
-    apiKey: "AIzaSyCbERhlqJFsrx9Czn28jdvzhlLHdes_YDs",
-    authDomain: "applocationimg.firebaseapp.com",
-    projectId: "applocationimg",
-    storageBucket: "applocationimg.appspot.com",
-    messagingSenderId: "161168490799",
-    appId: "1:161168490799:web:715923c03e597b72d921da"
+    apiKey: "AIzaSyDo3gA5ayjW2uY36Jzow-oLL96ddA7cM98",
+    authDomain: "app2022b.firebaseapp.com",
+    databaseURL: "https://app2022b-default-rtdb.firebaseio.com",
+    projectId: "app2022b",
+    storageBucket: "app2022b.appspot.com",
+    messagingSenderId: "430051088078",
+    appId: "1:430051088078:web:d2489c60cfcc8d3af0629f",
+    measurementId: "G-18BCZEN1EZ"
   }
 };
 
